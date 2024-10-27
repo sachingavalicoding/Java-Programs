f36c4184626f3271757bf6daf94c116a2e356f09
Sachin Gavali <sachingavali321@gmail.com>
2024-10-27 20:45:13 +0530

    power

---
+++ Number Programming/Power.java
@@ -0,0 +1,9 @@
+class Power{
+    public static int getSq(int n){
+        return n * n;
+    }
+    public static void main(String[] args) {
+        int num = 121;
+        System.out.println(getSq(num));
+    }
+}
