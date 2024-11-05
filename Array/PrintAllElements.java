43ebf31fc64565fa71246c9dc690e1a5795f72bb
Sachin Gavali <sachingavali321@gmail.com>
2024-11-05 11:27:43 +0530

    array

---
+++ Array/PrintAllElements.java
@@ -0,0 +1,9 @@
+class PrintAllElements{
+    public static void main(String[] args) {
+        int [] arr = {10,43,34,343,23};
+
+        for(int i = 0; i < arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
